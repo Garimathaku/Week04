6364206abea84e7ca140e1a0850f5fbe39ce1be2
Garimathaku <garimathakur397@gmail.com>
2025-02-14 15:11:23 +0530

    [Garima Thakur] Add week4 task Bank Transaction System Problem

---
+++ src/main/java/com/week4/day4/BankTransactionSystem.java
@@ -0,0 +1,64 @@
+package com.week4.day4;
+
+
+// Custom Checked Exception
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+//BankAccount class
+class BankAccount {
+    private double balance;
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    // Withdraw method
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+
+    // Getter for balance
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class  BankTransactionSystem {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(5000); // Initial balance: 5000
+
+        try {
+            account.withdraw(7000);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            account.withdraw(-1000);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            account.withdraw(2000); // Valid withdrawal
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
